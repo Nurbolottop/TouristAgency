341f4938d67de359bad97f3e8ce3dffb83e40172
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-01-16 09:39:01 -0600

    0.001%

--- apps/tours/models.py
+++ apps/tours/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 class Tours(models.Model):
     image_tour = models.ImageField(upload_to="Tours", verbose_name="Фотография тура")
-    name_tour = models.CharField(max_length=255,verbose_name=)
+    name_tour = models.CharField(max_length=255,verbose_name="Название тура")
